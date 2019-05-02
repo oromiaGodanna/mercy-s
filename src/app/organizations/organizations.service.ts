9d68e333dab0ab15204ae9ed371e219811d06660
Mihret Tamene <tamenemihret@gmail.com>
2019-05-02 09:16:24 +0300

    setup organizations service

--- src/app/organizations/organizations.service.ts
+++ src/app/organizations/organizations.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { Organization } from './organizations.model';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrganizationsService {
 
+  organizationListChanged = new Subject<Organization[]>();
+
+  private orgnizations: Organization[] = [
+    new Organization("Organization1", "Organization 1", 1),
+    new Organization("Organization2", "Organization 2", 2),
+    new Organization("Organization3", "Organization 3", 3),
+    new Organization("Organization4", "Organization 4", 4),
+    new Organization("Organization5", "Organization 5", 5),
+  ];
+
   constructor() { }
+
+
+  getOrganizations(){
+    return this.orgnizations.slice();
+  }
+
+  getOrganization(id: number){
+    return this.orgnizations.find(organization => organization.id == id);
+  }
+
+  addOrganization(newOrganization: Organization){
+    this.orgnizations.push(newOrganization);
+    this.organizationListChanged.next(this.orgnizations.slice());
+  }
+
+  updateOrganization(id: number, newOrganization: Organization){
+    var index = this.orgnizations.indexOf(this.getOrganization(id));
+    this.orgnizations[index] = newOrganization;
+    this.organizationListChanged.next(this.orgnizations.slice());
+  }
+
+  deleteOrganization(id: number){
+    var index = this.orgnizations.indexOf(this.getOrganization(id));
+    this.orgnizations.splice(index, 1);
+    this.organizationListChanged.next(this.orgnizations.slice());
+  }
 }
