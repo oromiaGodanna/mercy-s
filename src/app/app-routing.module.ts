3e7602b59cf98406bb0a64384ae79b2f4a3a5643
Mihret Tamene <tamenemihret@gmail.com>
2019-05-02 12:32:22 +0300

    departmnet routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { OrganizationsComponent } from "./organizations/organizations.component";
 import { OrganizationsFormComponent } from './organizations/organizations-form/organizations-form.component';
 import { OrganizationsDetailComponent } from './organizations/organizations-detail/organizations-detail.component';
+import { AllDepartmentsComponent } from './departments/all-departments/all-departments.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { DepartmentsFormComponent } from './departments/departments-form/departments-form.component';
+import { DepartmentsDetailComponent } from './departments/departments-detail/departments-detail.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo:'/organizations', pathMatch: 'full'},
@@ -11,6 +15,11 @@ const appRoutes: Routes = [
     { path: "org-form", component: OrganizationsFormComponent },
     { path: "org-form/:id", component: OrganizationsFormComponent},
     { path: "org-details/:id", component: OrganizationsDetailComponent}, 
+    { path: "all-departments", component: AllDepartmentsComponent},
+    { path: "departments", component: DepartmentsComponent},
+    { path: "dept-form", component: DepartmentsFormComponent},
+    { path: "dept-form/:id", component: DepartmentsFormComponent},
+    { path: "dept-details/:id", component: DepartmentsDetailComponent}
 ];
 
 @NgModule({
