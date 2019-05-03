65287486af5725f014de64631828cff81a4507c3
Mihret Tamene <tamenemihret@gmail.com>
2019-05-03 16:30:32 +0300

    department Details component

--- src/app/departments/departments-detail/departments-detail.component.ts
+++ src/app/departments/departments-detail/departments-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Department } from '../departmnets.model';
+import { DepartmentsService } from '../departments.service';
+import { OrganizationsService } from 'src/app/organizations/organizations.service';
 
 @Component({
   selector: 'app-departments-detail',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentsDetailComponent implements OnInit {
 
-  constructor() { }
+  id:number;
+  department: Department;
+  constructor(private departmentService: DepartmentsService,
+              private organizationService: OrganizationsService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = params['id'];
+        this.department = this.departmentService.getDepartment(this.id);
+        this.initializeData() 
+      });
+  }
+  private initializeData() {
+    this.department.organizationName = this.organizationService.getOrganization(this.department.organization_id).name;
+      if (this.department.parent_id != null) {
+        this.department.parentName = this.departmentService.getDepartment(this.department.parent_id).name;
+      } else {
+        this.department.parentName = "None";
+      }
   }
 
 }
